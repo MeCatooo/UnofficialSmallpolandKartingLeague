ead765ff3cd7ac14681e578d609cfcfc02eef3ee
MeCatooo <adam.turek@microsoft.wsei.edu.pl>
2024-08-11 19:42:51 +0200

    Block desktop users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    console.log(this.isValid)
+  }
   title = 'UnofficialSmallpolandKartingLeague';
   opened: boolean = false;
+  isValid = screen.width < 800;
 }
